fdf37872d68cbe3c058296ea9b2f57f9b86d825d
realloxer <realloxer@gmail.com>
2018-11-20 17:44:36 +0700

    Resizable crop frame

--- CropImage/BBMCropRotateImageViewController.swift
+++ CropImage/BBMCropRotateImageViewController.swift
@@ -8,26 +8,25 @@
 
 import UIKit
 
+let imageName = "Image3"
+
 class BBMCropRotateImageViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
     @objc public var image: UIImage! {
         didSet {
             self.imageView.image = image
-            let width = image.size.width
-            let height = image.size.height
-            self.imageView.frame = CGRect(x: 0, y:0, width: width, height: height)
-            self.imageView.center = self.view.center;
+            self.calibrateImageView()
         }
     }
+    var originalImage: UIImage?
     let imageView = CroppableImageView()
-    let cropView = UIView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.image = UIImage(named: "Image2")
+        self.originalImage = UIImage(named: imageName)
+        self.image = UIImage(named: imageName)
         self.setupUI()
-        self.setupGesture()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -43,33 +42,10 @@ class BBMCropRotateImageViewController: UIViewController, UIScrollViewDelegate {
         self.scrollView.maximumZoomScale = 2.0
         self.scrollView.isHidden = true
         
-//        self.imageView.frame = self.scrollView.bounds
-//        self.imageView.autoresizingMask = UIViewAutoresizing(rawValue: 0xFF)  //Fill, stretch and scale
         self.imageView.contentMode = .scaleAspectFit
         self.imageView.isUserInteractionEnabled = true
         self.view.addSubview(self.imageView)
         self.view.sendSubview(toBack: self.imageView)
-//        self.setupCropView()
-    }
-    
-    func setupCropView() {
-        self.cropView.frame = CGRect(x: 0, y: 0, width: self.scrollView.frame.size.width / 2, height: self.scrollView.frame.size.width / 2)
-        self.cropView.backgroundColor = .clear
-        self.cropView.layer.borderWidth = 2
-        self.cropView.layer.borderColor = UIColor.red.cgColor
-            self.cropView.center = CGPoint(x: self.scrollView.frame.size.width / 2, y: self.scrollView.frame.size.height / 2)
-            self.imageView.addSubview(self.cropView)
-    }
-    
-    func setupGesture() {
-        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(self.zoomImage))
-        doubleTap.numberOfTapsRequired = 2
-        self.view.addGestureRecognizer(doubleTap)
-        
-        let panRecognizer = UIPanGestureRecognizer(target:self, action:#selector(self.move(sender:)))
-        panRecognizer.minimumNumberOfTouches = 1
-        panRecognizer.maximumNumberOfTouches = 1
-        self.cropView.addGestureRecognizer(panRecognizer)
     }
     
     @objc func zoomImage() {
@@ -80,41 +56,10 @@ class BBMCropRotateImageViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
-    @objc func move(sender: UIPanGestureRecognizer) {
-        guard let senderView = sender.view else { return }
-        self.view.bringSubview(toFront: senderView)
-        var translatedPoint = sender.translation(in: senderView.superview)
-        
-        translatedPoint = CGPoint(x: senderView.center.x + translatedPoint.x, y: senderView.center.y + translatedPoint.y)
-        var finalX = translatedPoint.x
-        var finalY = translatedPoint.y
-        let senderViewHalfWidth = senderView.frame.size.width / 2
-        let senderViewHalfHeight = senderView.frame.size.height / 2
-        
-        if finalX <= senderViewHalfWidth {
-            finalX = senderViewHalfWidth
-        } else if finalX >= self.view.frame.size.width - senderViewHalfWidth {
-            finalX = self.view.frame.size.width - senderViewHalfWidth
-        }
-        
-        if finalY <= senderViewHalfHeight {
-            finalY = senderViewHalfHeight
-        } else if finalY >= self.view.frame.size.height - senderViewHalfHeight {
-            finalY = self.view.frame.size.height - senderViewHalfHeight
-        }
-        senderView.center = CGPoint(x: finalX, y: finalY)
-        
-        sender.setTranslation(.zero, in:senderView)
-    }
-    
     // MARK: User Interactions
     @IBAction func rotateButtonTouched(_ sender: Any) {
-        guard let image = self.imageView.image else { return }
-        let radians = atan2f(Float(self.imageView.transform.b), Float(self.imageView.transform.a))
-        let newImage = self.rotateImage(image, angle:CGFloat(radians - .pi / 2))
-        UIView.animate(withDuration: 0.5) {
-            self.imageView.image = newImage
-        }
+        self.imageView.rotate()
+        self.calibrateImageView()
     }
     
     @IBAction func doneButtonTouched(_ sender: Any) {
@@ -122,24 +67,8 @@ class BBMCropRotateImageViewController: UIViewController, UIScrollViewDelegate {
     }
     
     func cropImage() {
-        //Failed
-//        UIGraphicsBeginImageContext(self.cropView.frame.size)
-//        if let context = UIGraphicsGetCurrentContext() {
-//            self.cropView.layer.draw(in: context)
-//        }
-//        let screenShot = UIGraphicsGetImageFromCurrentImageContext();
-//        UIGraphicsEndImageContext();
-//        self.imageView.image = screenShot;
-        
-//        let croppedRect = self.cropView.convert(self.cropView.frame, to: self.imageView)
-//        if let cgImage = self.imageView.image?.cgImage {
-//            if let subImage = cgImage.cropping(to: croppedRect) {
-//                let croppedImage = UIImage(cgImage: subImage)
-//                self.imageView.image = croppedImage;
-//            }
-//        }
-        
         self.imageView.cropImage()
+        self.calibrateImageView()
     }
     
     @IBAction func cropButtonTouched(_ sender: Any) {
@@ -147,38 +76,34 @@ class BBMCropRotateImageViewController: UIViewController, UIScrollViewDelegate {
     }
     
     @IBAction func resetButtonTouched(_ sender: Any) {
-        self.image = UIImage(named: "Image2")
+        self.image = self.originalImage
+        self.imageView.resetState()
     }
     
     @IBAction func cancelButtonTouched(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
-    
-    func rotateImage(_ image: UIImage, angle: CGFloat) -> UIImage  {
-        let ciImage = CIImage(image:image)
-        guard let filter = CIFilter(name:"CIAffineTransform") else { return UIImage() }
-        
-        filter.setValue(ciImage, forKey:kCIInputImageKey)
-        filter.setDefaults()
-        
-        let newAngle = angle * -1.0
-        
-        var transform = CATransform3DIdentity
-        transform = CATransform3DRotate(transform, newAngle, 0, 0, 1)
-        
-        let affineTransform = CATransform3DGetAffineTransform(transform)
-        filter.setValue(NSValue(cgAffineTransform: affineTransform), forKey: "inputTransform")
-        
-        let contex = CIContext(options: [kCIContextUseSoftwareRenderer: true])
-        
-        guard let outputImage = filter.outputImage, let cgImage = contex.createCGImage(outputImage, from:outputImage.extent) else {
-            return UIImage()
-        }
-        let result = UIImage(cgImage: cgImage)
-        return result
-    }
-    
+}
+
+extension BBMCropRotateImageViewController {
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return self.imageView
+    }
+    
+    func calibrateImageView() {
+        guard let assignedImage = self.imageView.image else { return }
+        var width = assignedImage.size.width
+        var height = assignedImage.size.height
+        let viewSize = self.view.frame.size
+        let ratio = height / width
+        width = viewSize.width
+        height = width * ratio
+        if height > viewSize.height {
+            height = viewSize.height
+            width = height / ratio
+        }
+        
+        self.imageView.frame = CGRect(x: 0, y:0, width: width, height: height)
+        self.imageView.center = self.view.center;
     }
 }
--- CropImage/CroppableImageView.swift
+++ CropImage/CroppableImageView.swift
@@ -8,8 +8,22 @@
 
 import UIKit
 
+let kTouchValidatePaddingRation: CGFloat = 0.2
+
+enum CropFrameAction : Int {
+    case None = 0
+    case Move = 1
+    case Resize = 2
+}
+
 class CroppableImageView: UIImageView {
-    let cropView = UIView()
+    private let cropView = UIView()
+    private var firstTouchLocation: CGPoint?
+    var defaultMinimumFrameSize: CGFloat = 50.0
+    var defaultCropFrame = CGRect(x: 0, y: 0, width: 100, height: 100)
+    var changedEdge: UIRectEdge?
+    var cropAction = CropFrameAction.None
+    var validatingTouchingFrame: CGRect?
 
     init() {
         super.init(frame: .zero)
@@ -30,32 +44,139 @@ class CroppableImageView: UIImageView {
         self.cropView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
         self.cropView.backgroundColor = .clear
         self.cropView.layer.borderWidth = 2
-        self.cropView.layer.borderColor = UIColor.red.cgColor
-//        self.cropView.center = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+        self.cropView.layer.borderColor = UIColor.white.cgColor
         self.addSubview(self.cropView)
+        self.addGridView()
+        // Testing only
+        self.addDebugDragingFrame()
+    }
+    
+    func resetCropViewFrame() {
+        let size = min(self.frame.size.width, self.frame.size.height)
+        self.cropView.frame = CGRect(x: 0, y: 0, width: size, height: size)
+        self.cropView.center = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+    }
+    
+    func addDebugDragingFrame() {
+        let touchView = UIView(frame: .zero)
+        touchView.layer.borderColor = UIColor.red.cgColor
+        touchView.layer.borderWidth = 2
+        self.cropView.addSubview(touchView)
+        touchView.translatesAutoresizingMaskIntoConstraints = false
+        let horizontalConstraint = NSLayoutConstraint(item: touchView, attribute: .centerX, relatedBy: .equal, toItem: self.cropView, attribute: .centerX, multiplier: 1, constant: 0)
+        let verticalConstraint = NSLayoutConstraint(item: touchView, attribute: .centerY, relatedBy: .equal, toItem: self.cropView, attribute: .centerY, multiplier: 1, constant: 0)
+        let heightConstraint = NSLayoutConstraint(item: touchView, attribute: .height, relatedBy: .equal, toItem: self.cropView, attribute: .height, multiplier: 0.8, constant: 0)
+        let widthConstraint = NSLayoutConstraint(item: touchView, attribute: .width, relatedBy: .equal, toItem: self.cropView, attribute: .width, multiplier: 0.8, constant: 0)
+        self.cropView.addConstraints([horizontalConstraint, verticalConstraint, heightConstraint, widthConstraint])
+        self.cropView.layoutSubviews()
+    }
+    
+    func addGridView() {
+        let verticalGridView = UIView()
+        verticalGridView.layer.borderColor = UIColor.white.cgColor
+        verticalGridView.layer.borderWidth = 1
+        self.cropView.addSubview(verticalGridView)
+        verticalGridView.translatesAutoresizingMaskIntoConstraints = false
+        var horizontalConstraint = NSLayoutConstraint(item: verticalGridView, attribute: .centerX, relatedBy: .equal, toItem: self.cropView, attribute: .centerX, multiplier: 1, constant: 0)
+        var verticalConstraint = NSLayoutConstraint(item: verticalGridView, attribute: .centerY, relatedBy: .equal, toItem: self.cropView, attribute: .centerY, multiplier: 1, constant: 0)
+        var heightConstraint = NSLayoutConstraint(item: verticalGridView, attribute: .height, relatedBy: .equal, toItem: self.cropView, attribute: .height, multiplier: 1, constant: 0)
+        var widthConstraint = NSLayoutConstraint(item: verticalGridView, attribute: .width, relatedBy: .equal, toItem: self.cropView, attribute: .width, multiplier: 1/3, constant: 0)
+        self.cropView.addConstraints([horizontalConstraint, verticalConstraint, heightConstraint, widthConstraint])
+        
+        let horizontalGridView = UIView()
+        horizontalGridView.layer.borderColor = UIColor.white.cgColor
+        horizontalGridView.layer.borderWidth = 1
+        self.cropView.addSubview(horizontalGridView)
+        horizontalGridView.translatesAutoresizingMaskIntoConstraints = false
+        horizontalConstraint = NSLayoutConstraint(item: horizontalGridView, attribute: .centerX, relatedBy: .equal, toItem: self.cropView, attribute: .centerX, multiplier: 1, constant: 0)
+        verticalConstraint = NSLayoutConstraint(item: horizontalGridView, attribute: .centerY, relatedBy: .equal, toItem: self.cropView, attribute: .centerY, multiplier: 1, constant: 0)
+        heightConstraint = NSLayoutConstraint(item: horizontalGridView, attribute: .height, relatedBy: .equal, toItem: self.cropView, attribute: .height, multiplier: 1/3, constant: 0)
+        widthConstraint = NSLayoutConstraint(item: horizontalGridView, attribute: .width, relatedBy: .equal, toItem: self.cropView, attribute: .width, multiplier: 1, constant: 0)
+        self.cropView.addConstraints([horizontalConstraint, verticalConstraint, heightConstraint, widthConstraint])
     }
     
     func setupGesture() {
-//        let doubleTap = UITapGestureRecognizer(target: self, action: #selector(self.zoomImage))
-//        doubleTap.numberOfTapsRequired = 2
-//        self.addGestureRecognizer(doubleTap)
-        
-        let panRecognizer = UIPanGestureRecognizer(target:self, action:#selector(self.move(sender:)))
+        let panRecognizer = UIPanGestureRecognizer(target:self, action:#selector(self.handlePanMove(sender:)))
         panRecognizer.minimumNumberOfTouches = 1
         panRecognizer.maximumNumberOfTouches = 1
-        self.cropView.addGestureRecognizer(panRecognizer)
+        self.addGestureRecognizer(panRecognizer)
     }
     
-    @objc private func move(sender: UIPanGestureRecognizer) {
+    @objc private func handlePanMove(sender: UIPanGestureRecognizer) {
         guard let senderView = sender.view else { return }
-        self.bringSubview(toFront: senderView)
-        var translatedPoint = sender.translation(in: senderView.superview)
-        
-        translatedPoint = CGPoint(x: senderView.center.x + translatedPoint.x, y: senderView.center.y + translatedPoint.y)
+        let locationPoint = sender.location(in: senderView)
+        switch sender.state {
+        case .began:
+            self.assignActionForCropFrame(withPoint: locationPoint)
+        case .changed:
+            switch self.cropAction {
+            case .Move:
+                let translatedPoint = sender.translation(in: senderView)
+                self.moveCropFrame(touchedPoint: translatedPoint)
+            case .Resize:
+                self.resizeCropFrame(touchedPoint: locationPoint)
+            case .None:
+                break
+            }
+        case .ended:
+            self.cropAction = .None
+            
+        default:
+            break
+        }
+        sender.setTranslation(.zero, in:senderView)
+    }
+    
+    func assignActionForCropFrame(withPoint point: CGPoint) {
+        var cropFrame = self.cropView.frame
+        let deltaWidth = cropFrame.width * kTouchValidatePaddingRation
+        let deltaHeight = cropFrame.height * kTouchValidatePaddingRation
+        cropFrame = cropFrame.insetBy(dx: deltaWidth, dy: deltaHeight)
+        self.validatingTouchingFrame = cropFrame
+        if cropFrame.contains(point) {
+            self.prepareToMoveCropFrame(initialPoint: point)
+        } else {
+            self.prepareToResizeCropFrame(initialPoint: point)
+        }
+    }
+    
+    func prepareToMoveCropFrame(initialPoint point:CGPoint) {
+        self.bringSubview(toFront: self.cropView)
+        self.cropAction = .Move
+    }
+
+    func prepareToResizeCropFrame(initialPoint point:CGPoint) {
+        guard let cropFrame = self.validatingTouchingFrame else { return }
+        self.cropAction = .Resize
+        self.firstTouchLocation = point
+        var changedEdge: UIRectEdge = []
+        if point.x <= cropFrame.minX {
+            changedEdge.insert(.left)
+        } else if point.x >= cropFrame.maxX  {
+            changedEdge.insert(.right)
+        }
+        if point.y <= cropFrame.minY {
+            changedEdge.insert(.top)
+        } else if point.y >= cropFrame.maxY {
+            changedEdge.insert(.bottom)
+        }
+        self.changedEdge = changedEdge
+    }
+    
+    func endCropAction() {
+        self.cropAction = .None
+        self.firstTouchLocation = nil
+        self.changedEdge = nil
+    }
+    
+    func moveCropFrame(touchedPoint point: CGPoint) {
+        self.bringSubview(toFront: self.cropView)
+        let translatedPoint = CGPoint(x: self.cropView.center.x + point.x, y: self.cropView.center.y + point.y)
+
         var finalX = translatedPoint.x
         var finalY = translatedPoint.y
-        let senderViewHalfWidth = senderView.frame.size.width / 2
-        let senderViewHalfHeight = senderView.frame.size.height / 2
+        let senderViewHalfWidth = self.cropView.frame.size.width / 2
+        let senderViewHalfHeight = self.cropView.frame.size.height / 2
         
         if finalX <= senderViewHalfWidth {
             finalX = senderViewHalfWidth
@@ -68,27 +189,127 @@ class CroppableImageView: UIImageView {
         } else if finalY >= self.frame.size.height - senderViewHalfHeight {
             finalY = self.frame.size.height - senderViewHalfHeight
         }
-        senderView.center = CGPoint(x: finalX, y: finalY)
-        
-        sender.setTranslation(.zero, in:senderView)
+        self.cropView.center = CGPoint(x: finalX, y: finalY)
     }
     
-    func cropImage() {
-        //Failed
-        //        UIGraphicsBeginImageContext(self.cropView.frame.size)
-        //        if let context = UIGraphicsGetCurrentContext() {
-        //            self.cropView.layer.draw(in: context)
-        //        }
-        //        let screenShot = UIGraphicsGetImageFromCurrentImageContext();
-        //        UIGraphicsEndImageContext();
-        //        self.imageView.image = screenShot;
+    func resizeCropFrame(touchedPoint touchPoint: CGPoint) {
+        guard let firstTouchLocation = self.firstTouchLocation,
+            let changedEdge = self.changedEdge else {
+            return
+        }
+        var cropFrame = self.cropView.frame
+
+        let deltaX = changedEdge.contains(.left) || changedEdge.contains(.right) ? touchPoint.x - firstTouchLocation.x : 0
+        let deltaY = changedEdge.contains(.top) || changedEdge.contains(.bottom) ? touchPoint.y - firstTouchLocation.y : 0
         
-        let croppedRect = self.cropView.frame
+        if changedEdge.contains(.left) {
+            cropFrame.size.width -= deltaX
+            cropFrame.origin.x += deltaX
+            if cropFrame.origin.x < 0 {
+                let delta = -cropFrame.origin.x
+                cropFrame.origin.x = 0
+                cropFrame.size.width -= delta
+            }
+        }
+        if changedEdge.contains(.right) {
+            cropFrame.size.width += deltaX
+            if cropFrame.maxX > self.frame.width {
+                let delta = cropFrame.maxX - self.frame.width
+                cropFrame.size.width -= delta
+            }
+        }
+        
+        if changedEdge.contains(.top) {
+            cropFrame.size.height -= deltaY
+            cropFrame.origin.y += deltaY
+            if cropFrame.origin.y < 0 {
+                let delta = -cropFrame.origin.y
+                cropFrame.origin.y = 0
+                cropFrame.size.height -= delta
+            }
+        }
+        if changedEdge.contains(.bottom) {
+            cropFrame.size.height += deltaY
+            if cropFrame.maxY > self.frame.height {
+                let delta = cropFrame.maxY - self.frame.height
+                cropFrame.size.height -= delta
+            }
+        }
+        if (self.isCropFrameExceedLimit(cropFrame)) {
+            return
+        }
+        self.cropView.frame = cropFrame
+        self.cropView.layoutSubviews()
+        self.firstTouchLocation = touchPoint
+    }
+    
+    func isCropFrameExceedLimit(_ cropFrame: CGRect) -> Bool {
+        if (cropFrame.size.height < self.defaultMinimumFrameSize || cropFrame.size.width < self.defaultMinimumFrameSize) ||
+            (!self.bounds.contains(cropFrame)) {
+            return true
+        }
+        return false
+    }
+}
+
+extension CroppableImageView {
+    func cropImage() {
+        guard let image = self.image else { return }
+        var croppedRect = self.cropView.frame
+        var width = image.size.width
+        var height = image.size.height
+
+        width = croppedRect.size.width * (width / self.frame.width)
+        height = croppedRect.size.height * (height / self.frame.height)
+        
+        let imageViewPoint = self.cropView.frame.origin;
+        
+        let percentX = imageViewPoint.x / frame.size.width
+        let percentY = imageViewPoint.y / frame.size.height
+        
+        let imagePoint = CGPoint(x: image.size.width * percentX, y: image.size.height * percentY)
+        croppedRect = CGRect(x: imagePoint.x, y: imagePoint.y, width: width, height: height)
         if let cgImage = self.image?.cgImage {
             if let subImage = cgImage.cropping(to: croppedRect) {
                 let croppedImage = UIImage(cgImage: subImage)
                 self.image = croppedImage;
             }
         }
+    }
+    
+    func resetState() {
+        self.resetCropViewFrame()
+    }
+    
+    func rotate() {
+        guard let image = self.image else { return }
+        let radians = atan2f(Float(self.transform.b), Float(self.transform.a))
+        let newImage = self.rotateImage(image, angle:CGFloat(radians - .pi / 2))
+        self.image = newImage
+    }
+    
+    // MARK: Private Methods
+    private func rotateImage(_ image: UIImage, angle: CGFloat) -> UIImage  {
+        let ciImage = CIImage(image:image)
+        guard let filter = CIFilter(name:"CIAffineTransform") else { return UIImage() }
+        
+        filter.setValue(ciImage, forKey:kCIInputImageKey)
+        filter.setDefaults()
+        
+        let newAngle = angle * -1.0
+        
+        var transform = CATransform3DIdentity
+        transform = CATransform3DRotate(transform, newAngle, 0, 0, 1)
+        
+        let affineTransform = CATransform3DGetAffineTransform(transform)
+        filter.setValue(NSValue(cgAffineTransform: affineTransform), forKey: "inputTransform")
+        
+        let contex = CIContext(options: [kCIContextUseSoftwareRenderer: true])
+        
+        guard let outputImage = filter.outputImage, let cgImage = contex.createCGImage(outputImage, from:outputImage.extent) else {
+            return UIImage()
+        }
+        let result = UIImage(cgImage: cgImage)
+        return result
     }
 }
